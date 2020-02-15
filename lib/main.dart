4312439858de42149fdf36c27ef36cde81cccdb6
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2020-02-15 20:48:30 +0100

    l10n with 3rd party lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:openflutterecommerce/config/routes.dart';
 import 'package:openflutterecommerce/config/theme.dart';
 import 'package:openflutterecommerce/screens/categories/categories_screen.dart';
@@ -13,6 +14,7 @@ import 'authentication/authentication.dart';
 import 'config/routes.dart';
 import 'screens/home/home_screen.dart';
 import 'screens/signin/signin.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 
 class SimpleBlocDelegate extends BlocDelegate {
   @override
@@ -34,12 +36,17 @@ class SimpleBlocDelegate extends BlocDelegate {
   }
 }
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   BlocSupervisor.delegate = SimpleBlocDelegate();
+  var delegate = await LocalizationDelegate.create(
+      fallbackLocale: 'en_US', supportedLocales: ['en_US', 'de']);
   runApp(BlocProvider<AuthenticationBloc>(
     create: (context) => AuthenticationBloc()..add(AppStarted()),
-    child: OpenFlutterEcommerceApp(),
+    child: LocalizedApp(
+      delegate,
+      OpenFlutterEcommerceApp(),
+    ),
   ));
 }
 
@@ -61,30 +68,42 @@ class OpenFlutterEcommerceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Open FLutter E-commerce',
-      theme: OpenFlutterEcommerceTheme.of(context),
-      routes: <String, WidgetBuilder>{
-        OpenFlutterEcommerceRoutes.home: (context) => HomeScreen(),
-        OpenFlutterEcommerceRoutes.cart: (context) => HomeScreen(),
-        OpenFlutterEcommerceRoutes.favourites: (context) => HomeScreen(),
-        OpenFlutterEcommerceRoutes.SIGNIN: (context) => _signIn,
-        OpenFlutterEcommerceRoutes.SIGNUP: (context) => _signUp,
-        OpenFlutterEcommerceRoutes.FORGET_PASSWORD: (context) =>
-            _forgetPassword,
-        OpenFlutterEcommerceRoutes.shop: (context) => CategoriesScreen(),
-        OpenFlutterEcommerceRoutes.profile: (context) =>
-            BlocBuilder<AuthenticationBloc, AuthenticationState>(
-                builder: (context, state) {
-              if (state is Authenticated) {
-                return HomeScreen(); //TODO profile properties should be here
-              } else if (state is Unauthenticated) {
-                return _signUp;
-              } else {
-                return SplashScreen();
-              }
-            }),
-      },
+    var localizationDelegate = LocalizedApp.of(context).delegate;
+
+    return LocalizationProvider(
+      state: LocalizationProvider.of(context).state,
+      child: MaterialApp(
+        title: 'Open FLutter E-commerce',
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          localizationDelegate
+        ],
+        supportedLocales: localizationDelegate.supportedLocales,
+        locale: localizationDelegate.currentLocale,
+        theme: OpenFlutterEcommerceTheme.of(context),
+        routes: <String, WidgetBuilder>{
+          OpenFlutterEcommerceRoutes.home: (context) => HomeScreen(),
+          OpenFlutterEcommerceRoutes.cart: (context) => HomeScreen(),
+          OpenFlutterEcommerceRoutes.favourites: (context) => HomeScreen(),
+          OpenFlutterEcommerceRoutes.SIGNIN: (context) => _signIn,
+          OpenFlutterEcommerceRoutes.SIGNUP: (context) => _signUp,
+          OpenFlutterEcommerceRoutes.FORGET_PASSWORD: (context) =>
+              _forgetPassword,
+          OpenFlutterEcommerceRoutes.shop: (context) => CategoriesScreen(),
+          OpenFlutterEcommerceRoutes.profile: (context) =>
+              BlocBuilder<AuthenticationBloc, AuthenticationState>(
+                  builder: (context, state) {
+                if (state is Authenticated) {
+                  return HomeScreen(); //TODO profile properties should be here
+                } else if (state is Unauthenticated) {
+                  return _signUp;
+                } else {
+                  return SplashScreen();
+                }
+              }),
+        },
+      ),
     );
   }
 }
