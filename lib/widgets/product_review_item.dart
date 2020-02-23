1fda474a069b078a2e612497b8aec5a826157a7d
jagu <jagu@clarika.com.ar>
2020-02-23 11:48:28 -0300

    finish product detail

--- lib/widgets/product_review_item.dart
+++ lib/widgets/product_review_item.dart
@@ -6,17 +6,28 @@ class OpenFlutterProductReviewItem extends StatelessWidget {
   final double avatarSize;
   final String writerName;
   final String reviewDateTime;
-  final String description;
+  final String comment;
   final String avatarUrl;
+
+  final bool withPhotos;
+  final List<String> photos;
+  final double photoSize;
+
+  final double rating;
+  final int ratingCount;
 
   const OpenFlutterProductReviewItem({
     Key key,
     this.avatarUrl = "",
     this.avatarSize = 40.0,
     this.writerName = "Anonimo",
-    this.reviewDateTime = "June 5, 2019",
-    this.description =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    this.ratingCount = 0,
+    this.rating = 0.0,
+    this.reviewDateTime = "",
+    this.comment = "",
+    this.withPhotos = false,
+    this.photos,
+    this.photoSize = 96.0,
   }) : super(key: key);
 
   @override
@@ -47,7 +58,8 @@ class OpenFlutterProductReviewItem extends StatelessWidget {
                       ),
                       child: _buildRatingInformation(context),
                     ),
-                    _buildReviewDescription(context),
+                    _buildReviewComment(context),
+                    _buildPhotos(context),
                   ],
                 ),
               ),
@@ -93,8 +105,8 @@ class OpenFlutterProductReviewItem extends StatelessWidget {
       children: <Widget>[
         OpenFlutterProductRating(
           showLabel: false,
-          ratingCount: 5,
-          rating: 3.0,
+          ratingCount: ratingCount,
+          rating: rating,
           editable: false,
           iconSize: 14.0,
           spacing: 1.0,
@@ -112,12 +124,69 @@ class OpenFlutterProductReviewItem extends StatelessWidget {
     );
   }
 
-  Widget _buildReviewDescription(BuildContext context) {
-    return Text(
-      description,
-      style: Theme.of(context).textTheme.bodyText2.copyWith(
-            fontSize: 14.0,
+  Widget _buildReviewComment(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Text(
+        comment,
+        style: Theme.of(context).textTheme.bodyText2.copyWith(
+              fontSize: 14.0,
+            ),
+        textAlign: TextAlign.start,
+      ),
+    );
+  }
+
+  Widget _buildPhotos(BuildContext context) {
+    if (withPhotos && photos != null && photos.isNotEmpty) {
+      return Padding(
+        padding: const EdgeInsets.only(
+          top: 8.0,
+        ),
+        child: SizedBox(
+          height: photoSize,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: _buildPhotosItems(),
           ),
+        ),
+      );
+    }
+
+    return SizedBox();
+  }
+
+  List<Widget> _buildPhotosItems() {
+    var list = List<Widget>.generate(
+        photos.length, (index) => _buildPhotoItem(photos[index]));
+    return list;
+  }
+
+  Widget _buildPhotoItem(String photoUrl) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        right: 12.0,
+      ),
+      child: SizedBox(
+        width: photoSize,
+        height: photoSize,
+        child: Image.network(
+          photoUrl,
+          fit: BoxFit.fill,
+          loadingBuilder: (BuildContext context, Widget child,
+              ImageChunkEvent loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Center(
+              child: CircularProgressIndicator(
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes
+                    : null,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
