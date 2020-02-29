8eae05b121f4c7628a34d9a90bd8776e7631f68e
codingoliver <oliverboamah@yahoo.com>
2020-02-29 18:13:32 +0000

    Add settings_state

---
+++ lib/screens/profile/settings_state.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:equatable/equatable.dart';
+import 'package:openflutterecommerce/repos/models/settings.dart';
+
+@immutable
+abstract class SettingsState extends Equatable {
+  final Settings settings;
+
+  SettingsState({this.settings});
+
+  @override
+  List<Object> get props => [settings];
+}
+
+class FullNameUpdatedState extends SettingsState {}
+
+class DateOfBirthUpdatedState extends SettingsState {}
+
+class NotifySalesUpdatedState extends SettingsState {}
+
+class NotifyArrivalsUpdatedSate extends SettingsState {}
+
+class NotifyDeliveryUpdatedState extends SettingsState {}
