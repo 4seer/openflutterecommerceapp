127f643069c4c52c4a740a4ff7208e9f026abbc4
codingoliver <oliverboamah@yahoo.com>
2020-03-04 22:21:00 +0000

    Add productparametervariant data_source, entity, and tests

---
+++ lib/local_db/product/data_sources/productparametervariant_data_source.dart
@@ -0,0 +1,41 @@
+import 'package:openflutterecommerce/local_db/data_source.dart';
+import 'package:openflutterecommerce/local_db/product/entities/productparametervariant_entity.dart';
+
+class ProductParameterVariantDataSource extends DataSource {
+  @override
+  Future<List<ProductParameterVariantEntity>> all() async {
+    await openDatabaseIfNotOpened();
+
+    final List<Map<String, dynamic>> maps = await db.query(tableName);
+
+    return List.generate(maps.length, (i) {
+      return ProductParameterVariantEntity(
+        id: maps[i]['id'],
+        title: maps[i]['title'],
+        productParameterId: maps[i]['productParameterId'],
+        additionalPrice: maps[i]['additionalPrice'],
+      );
+    });
+  }
+
+  @override
+  Future<ProductParameterVariantEntity> get(int id) async {
+    await openDatabaseIfNotOpened();
+
+    final List<Map<String, dynamic>> maps =
+        await db.query(tableName, where: '$primaryKey = ?', whereArgs: [id]);
+
+    return ProductParameterVariantEntity(
+      id: maps[0]['id'],
+      title: maps[0]['title'],
+      productParameterId: maps[0]['productParameterId'],
+      additionalPrice: maps[0]['additionalPrice'],
+    );
+  }
+
+  @override
+  String get tableName => 'ProductParameterVariant';
+
+  @override
+  String get primaryKey => 'id';
+}
---
+++ lib/local_db/product/tests/test_productparametervariant_data_source.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:openflutterecommerce/local_db/product/data_sources/productparametervariant_data_source.dart';
+import 'package:openflutterecommerce/local_db/product/entities/productparametervariant_entity.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  group('Test ProductParameterVariant Data Source', () {
+    ProductParameterVariantDataSource dataSource;
+
+    setUp(() async {
+      dataSource = ProductParameterVariantDataSource();
+    });
+
+    test('test: insert and get a record in ProductParameterVariant table',
+        () async {
+      ProductParameterVariantEntity data = ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      );
+      await dataSource.insert(data);
+
+      ProductParameterVariantEntity insertedData =
+          await dataSource.get(data.id);
+      print(insertedData);
+      expect(data == insertedData, true);
+    });
+
+    test('test: update a record in ProductParameterVariant table', () async {
+      ProductParameterVariantEntity data = ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      );
+      await dataSource.insert(data);
+
+      ProductParameterVariantEntity insertedData =
+          await dataSource.get(data.id);
+
+      ProductParameterVariantEntity dataToUpdate =
+          ProductParameterVariantEntity(
+        id: insertedData.id,
+        productParameterId: 1,
+        additionalPrice: 20.0,
+        title: 'title',
+      );
+
+      await dataSource.update(dataToUpdate);
+
+      ProductParameterVariantEntity updatedData =
+          await dataSource.get(dataToUpdate.id);
+      expect(dataToUpdate == updatedData, true);
+    });
+
+    test('test: delete all records in ProductParameterVariant table', () async {
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 2,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 50.0,
+      ));
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 3,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 40.0,
+      ));
+
+      await dataSource.deleteAll();
+
+      List<ProductParameterVariantEntity> allRecords = await dataSource.all();
+      expect(allRecords.length == 0, true);
+    });
+
+    test('test: delete a record in ProductParameterVariant table', () async {
+      await dataSource.deleteAll();
+
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+
+      await dataSource.delete(1);
+
+      List<ProductParameterVariantEntity> allRecords = await dataSource.all();
+      expect(allRecords.length == 0, true);
+    });
+
+    test('test: get all records in ProductParameterVariant table', () async {
+      await dataSource.deleteAll();
+
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 2,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 3,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+
+      List<ProductParameterVariantEntity> allRecords = await dataSource.all();
+      expect(allRecords.length == 3, true);
+    });
+  });
+}
