af00d166e214a21e729907c35e5da67628a614b3
codingoliver <oliverboamah@yahoo.com>
2020-03-05 10:42:32 +0000

    Add user data_source, entity, and tests

---
+++ lib/local_db/user/data_sources/user_data_source.dart
@@ -0,0 +1,54 @@
+import 'package:openflutterecommerce/local_db/data_source.dart';
+import 'package:openflutterecommerce/local_db/user/entities/user_entity.dart';
+
+class UserDataSource extends DataSource {
+  @override
+  Future<List<UserEntity>> all() async {
+    await openDatabaseIfNotOpened();
+
+    final List<Map<String, dynamic>> maps = await db.query(tableName);
+
+    return List.generate(maps.length, (i) {
+      return UserEntity(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        avatar: maps[i]['avatar'],
+        email: maps[i]['email'],
+        password: maps[i]['password'],
+        birthDate: maps[i]['birthDate'],
+        salesNotification: maps[i]['salesNotification'].toString() == '1',
+        newArrivalsNotification:
+            maps[i]['newArrivalsNotification'].toString() == '1',
+        deliveryStatusChanges:
+            maps[i]['deliveryStatusChanges'].toString() == '1',
+      );
+    });
+  }
+
+  @override
+  Future<UserEntity> get(int id) async {
+    await openDatabaseIfNotOpened();
+
+    final List<Map<String, dynamic>> maps =
+        await db.query(tableName, where: '$primaryKey = ?', whereArgs: [id]);
+
+    return UserEntity(
+      id: maps[0]['id'],
+      name: maps[0]['name'],
+      avatar: maps[0]['avatar'],
+      email: maps[0]['email'],
+      password: maps[0]['password'],
+      birthDate: maps[0]['birthDate'],
+      salesNotification: maps[0]['salesNotification'].toString() == '1',
+      newArrivalsNotification:
+          maps[0]['newArrivalsNotification'].toString() == '1',
+      deliveryStatusChanges: maps[0]['deliveryStatusChanges'].toString() == '1',
+    );
+  }
+
+  @override
+  String get tableName => 'User';
+
+  @override
+  String get primaryKey => 'id';
+}
