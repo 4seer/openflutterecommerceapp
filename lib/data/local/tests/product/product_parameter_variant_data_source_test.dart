dcfebd920017ec2c9fbefe2999e69f88d9a91f5a
Andrew <openflutterproject@gmail.com>
2020-03-10 14:57:39 +0300

    Refactor: local databasetests.

---
+++ lib/data/local/tests/product/product_parameter_variant_data_source_test.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:openflutterecommerce/data/local/product/product_parameter_variant_data_source.dart';
+import 'package:openflutterecommerce/domain/entities/product/product_parameter_variant_entity.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  group('Test ProductParameterVariant Data Source', () {
+    ProductParameterVariantDataSource dataSource;
+
+    setUp(() async {
+      dataSource = ProductParameterVariantDataSource();
+      await dataSource.open();
+    });
+
+    test('test: insert and get a record in ProductParameterVariant table',
+        () async {
+      ProductParameterVariantEntity data = ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      );
+      await dataSource.insert(data);
+
+      ProductParameterVariantEntity insertedData =
+          await dataSource.get(data.id);
+      expect(data == insertedData, true);
+    });
+
+    test('test: update a record in ProductParameterVariant table', () async {
+      ProductParameterVariantEntity data = ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      );
+      await dataSource.insert(data);
+
+      ProductParameterVariantEntity insertedData =
+          await dataSource.get(data.id);
+
+      ProductParameterVariantEntity dataToUpdate =
+          ProductParameterVariantEntity(
+        id: insertedData.id,
+        productParameterId: 1,
+        additionalPrice: 20.0,
+        title: 'title',
+      );
+
+      await dataSource.update(dataToUpdate);
+
+      ProductParameterVariantEntity updatedData =
+          await dataSource.get(dataToUpdate.id);
+      expect(dataToUpdate == updatedData, true);
+    });
+
+    test('test: delete all records in ProductParameterVariant table', () async {
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 2,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 50.0,
+      ));
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 3,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 40.0,
+      ));
+
+      await dataSource.deleteAll();
+
+      List<ProductParameterVariantEntity> allRecords = await dataSource.all();
+      expect(allRecords.length == 0, true);
+    });
+
+    test('test: delete a record in ProductParameterVariant table', () async {
+      await dataSource.deleteAll();
+
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+
+      await dataSource.delete(1);
+
+      List<ProductParameterVariantEntity> allRecords = await dataSource.all();
+      expect(allRecords.length == 0, true);
+    });
+
+    test('test: get all records in ProductParameterVariant table', () async {
+      await dataSource.deleteAll();
+
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 1,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 2,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+      await dataSource.insert(ProductParameterVariantEntity(
+        id: 3,
+        title: 'title',
+        productParameterId: 1,
+        additionalPrice: 20.0,
+      ));
+
+      List<ProductParameterVariantEntity> allRecords = await dataSource.all();
+      expect(allRecords.length == 3, true);
+    });
+
+    tearDown(() async {
+      await dataSource.close();
+    });
+  });
+}
