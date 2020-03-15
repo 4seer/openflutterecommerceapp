6dabad3e0225db6417908fe06b0eef92f455f875
Yahhi <vkonuhova@gmail.com>
2020-03-15 10:01:26 +0300

    make press on header responsive for the whole line. It was almost impossible to click on "View all"

--- lib/presentation/widgets/block_header.dart
+++ lib/presentation/widgets/block_header.dart
@@ -9,7 +9,7 @@ class OpenFlutterBlockHeader extends StatelessWidget {
   final double width;
   final String title;
   final String linkText;
-  final Function onLinkTap;
+  final VoidCallback onLinkTap;
   final String description;
 
   const OpenFlutterBlockHeader(
@@ -25,39 +25,41 @@ class OpenFlutterBlockHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     var _theme = Theme.of(context);
     const rightLinkWidth = 100.0;
-    return Container(
+    return InkWell(
+      onTap: onLinkTap,
+      child: Container(
         padding: EdgeInsets.only(
             top: AppSizes.sidePadding, left: AppSizes.sidePadding),
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Container(
-                    width: width - rightLinkWidth,
-                    child: Text(title, style: _theme.textTheme.caption),
-                  ),
-                  linkText != null
-                      ? InkWell(
-                          onTap: (() => {onLinkTap()}),
-                          child: Container(
-                            width: rightLinkWidth,
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: Text(linkText,
-                                  style: _theme.textTheme.bodyText2),
-                            ),
-                          ),
-                        )
-                      : Container(),
-                ],
-              ),
-              description != null
-                  ? Text(
-                      description,
-                      style: _theme.textTheme.bodyText2,
-                    )
-                  : Container()
-            ]));
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  width: width - rightLinkWidth,
+                  child: Text(title, style: _theme.textTheme.caption),
+                ),
+                linkText != null
+                    ? Container(
+                        width: rightLinkWidth,
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child:
+                              Text(linkText, style: _theme.textTheme.bodyText2),
+                        ),
+                      )
+                    : Container(),
+              ],
+            ),
+            description != null
+                ? Text(
+                    description,
+                    style: _theme.textTheme.bodyText2,
+                  )
+                : Container()
+          ],
+        ),
+      ),
+    );
   }
 }
