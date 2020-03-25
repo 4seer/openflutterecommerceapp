9a9d4350ab2fc188c017f4e41de2b7add58b3db5
4seer <openflutterproject@gmail.com>
2020-03-25 13:00:17 +0300

    App Secrete Settings

---
+++ lib/config/secret.dart
@@ -0,0 +1,7 @@
+// This file contains secret settings of the app 
+// that you should not upload to git
+// The AppSecretSettings class contains sample fields 
+// used in the app
+class AppSecretSettings{
+  String woocommerceHost = '';
+}
