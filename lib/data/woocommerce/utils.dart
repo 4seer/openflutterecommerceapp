28dc3a6164f3b18308afe6544b56de187bda5643
Andrey <openflutterproject@gmail.com>
2020-04-10 20:51:56 +0300

    Relocation of user login classes.

---
+++ lib/data/woocommerce/utils.dart
@@ -0,0 +1,28 @@
+/*
+ * @author Martin Appelmann <exlo89@gmail.com>
+ * @copyright 2020 Open E-commerce App
+ * @see utils.dart
+ */
+
+
+import 'package:openflutterecommerce/config/server_addresses.dart';
+import 'package:openflutterecommerce/config/storage.dart';
+
+class HttpClient {
+
+  Map createHeader() {
+    var header = <String, String>{
+      'authorization': 'Bearer ' + Storage().token,
+    };
+    return header;
+  }
+
+  Uri createUri(String route, [Map<String, String> param = const {}]) {
+    return Uri(
+      scheme: 'https',
+      host: ServerAddresses.serverAddress,
+      path: route,
+      queryParameters: param,
+    );
+  }
+}
