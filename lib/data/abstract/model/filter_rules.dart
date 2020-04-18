6b06a8931e12235f51f1dae3476b04f56b19b4ab
Andrey <openflutterproject@gmail.com>
2020-04-18 21:00:54 +0300

    Fix issues with empty attributes in products.

--- lib/data/abstract/model/filter_rules.dart
+++ lib/data/abstract/model/filter_rules.dart
@@ -28,7 +28,12 @@ class FilterRules {
 
   /// this behavior can be changed in subclasses to show special attribute instead of first
   MapEntry<ProductAttribute, List<String>> get topmostOption =>
-      selectedAttributes?.entries?.first;
+    selectedAttributes?.entries?.isNotEmpty ? 
+      selectedAttributes?.entries?.first 
+      : MapEntry<ProductAttribute, List<String>>(
+        ProductAttribute(name: ''),
+        []
+      );
 
   FilterRules copyWithRemovedAttributeValue(
       ProductAttribute attribute, String value) {
