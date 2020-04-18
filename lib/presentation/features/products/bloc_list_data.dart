9aefd5688467dc141722d971a3005ac82cfb0d7b
Andrey <openflutterproject@gmail.com>
2020-04-18 21:03:21 +0300

    Call use case from bloc instead of product repo.

--- lib/presentation/features/products/bloc_list_data.dart
+++ lib/presentation/features/products/bloc_list_data.dart
@@ -1,4 +1,5 @@
 import 'package:openflutterecommerce/data/abstract/model/category.dart';
+import 'package:openflutterecommerce/data/abstract/model/filter_rules.dart';
 import 'package:openflutterecommerce/data/abstract/model/hashtag.dart';
 import 'package:openflutterecommerce/data/abstract/model/product.dart';
 
@@ -6,10 +7,11 @@ class ProductListData {
   final List<Product> products;
   final List<HashTag> hashtags;
   final Category category;
+  final FilterRules filterRules;
 
-  ProductListData(this.products, this.hashtags, this.category);
+  ProductListData(this.products, this.hashtags, this.category, this.filterRules);
 
   ProductListData copyWith(List<Product> updatedProducts) {
-    return ProductListData(updatedProducts, hashtags, category);
+    return ProductListData(updatedProducts, hashtags, category, filterRules);
   }
 }
