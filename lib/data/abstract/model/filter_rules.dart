95d2680fe361437852789183a36a534f559fc481
Andrey <openflutterproject@gmail.com>
2020-04-28 12:37:16 +0300

    Product FilterRules: Get selectable attributes.

--- lib/data/abstract/model/filter_rules.dart
+++ lib/data/abstract/model/filter_rules.dart
@@ -1,10 +1,12 @@
 import 'dart:collection';
 
+import 'package:openflutterecommerce/data/abstract/model/product.dart';
+
 import 'category.dart';
 import 'product_attribute.dart';
 
 class FilterRules {
-  final HashMap<Category, bool> categories;
+  final HashMap<ProductCategory, bool> categories;
   final HashMap<ProductAttribute, List<String>> selectedAttributes;
   final PriceRange selectedPriceRange;
 
@@ -56,6 +58,53 @@ class FilterRules {
         categories: categories,
         selectedPriceRange: priceRange,
         selectedAttributes: selectedAttributes);
+  }
+
+  factory FilterRules.getSelectableAttributes(List<Product> products) {
+    HashMap<ProductAttribute, List<String>> returnAttributes = HashMap();
+    //hashmap to store option list by id
+    HashMap<int, List<String>> attributesIdToString = HashMap();
+    //hashmap to store ProductAttribute by id
+    HashMap<int, ProductAttribute> attributesIdToAttribute = HashMap();
+    //price ranges
+    double maxPrice = 0;
+    double minPrice = 0;
+    //TOOD: change to categories instead of categoryIds
+    List<int> categoryIds = [];
+    HashMap<ProductCategory, bool> categories = HashMap();
+    ///HashMap<String, int> attributeTitleToIndex = HashMap(); 
+
+    products.forEach((product) => {
+      product.selectableAttributes != null ?
+       // returnAttributes.addAll({for (var attribute in product.selectableAttributes) attribute: []})
+        product.selectableAttributes.forEach((attribute)=> {
+          if ( attribute != null ) { 
+            if ( attributesIdToString[attribute.id] == null ) {
+              attributesIdToString[attribute.id] = [],
+              attributesIdToAttribute[attribute.id] = attribute
+            },
+            attributesIdToString[attribute.id].addAll(attribute.options)
+          }
+        }) : { },
+      if ( product.price > maxPrice ) maxPrice = product.price,
+      if ( product.price < minPrice ) minPrice = product.price,
+      //TOOD: change to categories instead of categoryIds
+      product.categories.forEach((ProductCategory category) =>  {
+        if ( !categoryIds.contains(category.id) ) {
+          categoryIds.add(category.id),
+          categories[category] = true
+        }
+      })
+    });
+    attributesIdToString.keys.forEach((attributeById) => {
+      returnAttributes[attributesIdToAttribute[attributeById]]=attributesIdToString[attributeById].toSet().toList()
+    });
+
+    return  FilterRules(
+        categories: categories,
+        selectedAttributes: returnAttributes,
+        selectedPriceRange: PriceRange(minPrice, maxPrice)
+    );
   }
 }
 
