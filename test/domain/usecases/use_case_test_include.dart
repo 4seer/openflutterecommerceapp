2f1052bfb3c86398ba34bc970f2ded9a1ec6f365
Andrew <openflutterproject@gmail.com>
2020-05-05 16:06:02 +0300

    Woocommerce and use case tests.

---
+++ test/domain/usecases/use_case_test_include.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/mockito.dart';
+import 'package:openflutterecommerce/data/abstract/category_repository.dart';
+import 'package:openflutterecommerce/data/abstract/product_repository.dart';
+import 'package:openflutterecommerce/data/network/network_status.dart';
+import 'package:openflutterecommerce/data/woocommerce/repositories/category_remote_repository.dart';
+import 'package:openflutterecommerce/data/woocommerce/repositories/product_remote_repository.dart';
+import 'package:openflutterecommerce/data/woocommerce/repositories/woocommerce_wrapper.dart';
+
+class MockWoocommerceWrapper extends Mock implements WoocommercWrapperAbstract { }
+
+final sl = GetIt.instance; 
+
+void setupLocator() {
+  sl.allowReassignment = true;
+  
+  MockWoocommerceWrapper woocommerce = MockWoocommerceWrapper();
+
+  sl.registerLazySingleton<MockWoocommerceWrapper>(
+    () => woocommerce,
+  );
+  
+  sl.registerLazySingleton<WoocommercWrapperAbstract>(
+    () => woocommerce,
+  );
+  
+  sl.registerLazySingleton<CategoryRepository>(
+    () => RemoteCategoryRepository(woocommerce: woocommerce),
+  );
+
+  sl.registerLazySingleton<ProductRepository>(
+    () => RemoteProductRepository(woocommerce: sl()),
+  );
+  
+  sl.registerLazySingleton<NetworkStatus>(() => NetworkStatusImpl(DataConnectionChecker()));
+}
--- test/tests.dart
+++ test/tests.dart
@@ -1,7 +1,16 @@
-import '../test/data/local/all_tests.dart' as local_database_test;
-import '../test/data/woocommerce/woocommerce_tests.dart' as woocommerce_test;
+//import 'data/local/all_tests.dart' as local_database_test;
+import 'data/abstract/all_tests.dart' as abstract_test;
+import 'data/woocommerce/woocommerce_tests.dart' as woocommerce_test;
+import 'domain/usecases/all_use_case_test.dart' as use_case_test;
 
-void main(List<String> args) {
-  local_database_test.main(args);
-  woocommerce_test.main(args);
+void main() {
+  //run local database tests using
+  //command: flutter run -t test/data/local/all_tests.dart
+  //await local_database_test.main(args);
+
+  //run all other tests using 
+  //command: flutter test test/tests.dart
+  use_case_test.main();
+  woocommerce_test.main();
+  abstract_test.main();
 }
